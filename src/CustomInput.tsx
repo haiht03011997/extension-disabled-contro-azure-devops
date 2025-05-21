002308c999a4a9c0f21d95a2bc4f53571653da7a
haiht <haiht0301@gmail.com>
2025-05-21 09:35:11 +0700

    update code

--- src/CustomInput.tsx
+++ src/CustomInput.tsx
@@ -56,7 +56,7 @@ const CustomInput: React.FC = () => {
       title={value}
       onChange={handleChange}
       placeholder="Nhập giá trị"
-      disabled={isDisabled}
+      readOnly={isDisabled}
       className="w-100"
     />
   );
