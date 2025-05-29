86753e76059f144b971e7a543ca816eabbd93c1c
haiht <haiht0301@gmail.com>
2025-05-29 14:46:37 +0700

    update c

--- src/CustomInput.tsx
+++ src/CustomInput.tsx
@@ -5,19 +5,20 @@ import { getService } from "azure-devops-extension-sdk";
 import "./style.scss";
 import { Input } from "antd";
 
+const ORG = process.env.REACT_APP_AZURE_ORG!;
+const PAT = process.env.REACT_APP_AZURE_PAT!;
+const PROJECT = process.env.REACT_APP_AZURE_PROJECT!;
+const authHeader = "Basic " + PAT;
+
 const CustomInput: React.FC = () => {
   const [value, setValue] = React.useState<string>("");
-  const [fieldName, setFieldName] = React.useState<string>("");
-  const [label, setLabel] = React.useState<string>("");
-  const [isDisabled, setDisabled] = React.useState<boolean>(false);
-  const [isShow, setShow] = React.useState<boolean>(false);
 
   React.useEffect(() => {
     SDK.init();
 
     SDK.ready().then(async () => {
       const config = SDK.getConfiguration();
-      
+      const field = config.witInputs?.Field;
       // ✅ Đăng ký onFieldChanged
       SDK.register(SDK.getContributionId(), {
         onFieldChanged: async (args: any) => {
@@ -25,76 +26,75 @@ const CustomInput: React.FC = () => {
           const formService = await getService<IWorkItemFormService>(
             WorkItemTrackingServiceIds.WorkItemFormService
           );
-          if(formService)
-          {
-            if (changed["Custom.a2a2331d-644d-479d-b7fa-42698a4a8af0"]) {
-              const currentValue = await formService.getFieldValue("Custom.a2a2331d-644d-479d-b7fa-42698a4a8af0") as string;
-              if( currentValue.toLowerCase() === "CBNV".toLowerCase()) 
-                setShow(true);
-              else
-                setShow(false);
+           
+          if(formService && changed["System.RelatedLinkCount"] >= 0) {
+            const relatedLinkCount = await formService.getFieldValue("System.RelatedLinkCount") as number;
+            
+            if(relatedLinkCount <= 0) {
+              await formService.setFieldValue(field, "");
+              setValue("");
+            }
+            else {
+              const parrentId:number = await formService.getFieldValue("System.Parent") as number;
+              
+              if(parrentId && parrentId > 0)
+              {
+                const parent = await handleGetInforParent(parrentId);
+                let newValue: any;
+
+                if (field === "Custom.83cc03ff-b4cd-4863-9fbf-254247a6d8cf") {
+                  newValue = parent["System.AssignedTo"]["uniqueName"];
+                }
+                else if (field === "Custom.AreaPath") {
+                  await formService.setFieldValue("System.AreaPath", parent["System.AreaPath"]);
+                  newValue = parent[field];
+                }
+                else {
+                  newValue = parent[field];
+                }
+
+                await formService.setFieldValue(field, newValue);
+                setValue(newValue);
+              }
+              else {
+                await formService.setFieldValue(field, "");
+              }
             }
           }
-        },
-        onLoaded: async () => {
-          const formService = await getService<IWorkItemFormService>(
-            WorkItemTrackingServiceIds.WorkItemFormService
-          );
-          const initialState : string = await formService.getFieldValue("System.State") as string;
-          const normalized = String(initialState ?? "").toLowerCase();
-          const disabledRaw = config.witInputs?.IsDisabled;
-          const configDisabled = disabledRaw === true || disabledRaw === "true";
-
-          const finalDisabled = normalized === "hoàn thành" ? true : configDisabled;
-          setDisabled(finalDisabled);
-        },
+        }
       });
 
-      const field = config.witInputs?.Field;
-      setFieldName(field);
-      
-      const label = config.witInputs?.Label;
-      setLabel(label);
-      const formService = await getService<IWorkItemFormService>(
-        WorkItemTrackingServiceIds.WorkItemFormService
-      );
-      // Lấy giá trị ban đầu
-      const rawValue = await formService.getFieldValue(field);
-      const valueStr = rawValue as string;
-      setValue(valueStr);
-
-      SDK.resize(undefined, 55);
+      SDK.resize(undefined, 42);
     });
   }, []);
 
-  const handleChange = async (e?: any) => {
-    const newValue = e?.target?.value ?? e;
-    setValue(newValue ?? "");
-
-    if (fieldName) {
-      const formService = await getService<IWorkItemFormService>(WorkItemTrackingServiceIds.WorkItemFormService);
-      await formService.setFieldValue(fieldName, newValue ?? "");
-      if(newValue && newValue.length > 0) 
-        await formService.clearError();
-      else
-        await formService.setError(`Trường ${label} là bắt buộc.`);
+  const handleGetInforParent = async (id:number) => {
+    const res = await fetch(
+      `https://dev.azure.com/${ORG}/${encodeURI(PROJECT)}/_apis/wit/workitems/${id}?api-version=7.1`,
+      {
+        headers: {
+          Authorization: authHeader,
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        }
+      }
+    );
+     if (!res.ok) {
+      const errorText = await res.text();
+      console.error("❌ Azure DevOps API error:", res.status, errorText);
+      return;
     }
-  };
+    const data = await res.json();
+    return data?.fields;
+  }
 
   return (
-    isShow &&
-    (
-      <div className="custom-input-container">
-        <label className={`${value ? '' : 'text-error'}`}>{label}</label>
-        <Input
-          value={value}
-          title={value}
-          onChange={handleChange}
-          placeholder="Nhập giá trị"
-          disabled={isDisabled}
-        />
-      </div>
-    )
+    <Input
+      value={value}
+      title={value}
+      placeholder="Nhập giá trị"
+      disabled={true}
+    />
   );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,16 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+
+// Load biến từ .env
+const env = dotenv.config().parsed;
+
+// Chuyển thành object mà DefinePlugin hiểu được:
+const envKeys = env ? Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {}) : {};
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -25,14 +36,20 @@ module.exports = {
           "sass-loader"
         ],
         exclude: /node_modules/
-      }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        include: /node_modules/, // xử lý file CSS trong thư viện
+      },
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
       filename: "index.html"
-    })
+    }),
+    new webpack.DefinePlugin(envKeys) // 👈 THÊM DÒNG NÀY
   ],
   devServer: {
     static: {
